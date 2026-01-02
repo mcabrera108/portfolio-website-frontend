73aad5a25c2e9ad1617161f3602294d60307f6b9
mcabrera108 <martin.cabrera103@gmail.com>
2026-01-01 19:48:24 -0600

    Update message

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -11,6 +11,11 @@ function HomeBanner() {
         <div className={style.heroContainer}>
           <div className={style.heroSubTitle}>Software Engineer</div>
         </div>
+        <div style={{ color: "white" }}>
+          Update 12/31/2025: Hello! Thanks for taking a look at my old portfolio
+          page. My website and projects will be undergoing major updates in the
+          upcoming weeks. Stay tuned for more!
+        </div>
       </section>
       <About />
     </>
